fa3ca964eb264498c3cde12eaef9d01048052213
gokudlg53 <minicarlosganador@gmail.com>
2026-05-29 21:10:04 -0400

    Update MainShortestPath.java

--- MainShortestPath.java
+++ MainShortestPath.java
@@ -1,36 +1,13 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainShortestPath {
     public static void main(String[] args) {
-        // 1. Backtracking General
-        System.out.println("--- Ejecutando Backtracking General ---");
-        BacktrackingProblem problem = new MyBacktrackingProblem(); 
-        AtomicBoolean foundSolution = new AtomicBoolean(false);
-        ConcurrentLinkedQueue<Object> solutions = new ConcurrentLinkedQueue<>();
-        List<Integer> initialMoves = problem.getPossibleMoves();
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        List<ParallelBacktracking> tasks = new ArrayList<>();
-
-        for (int move : initialMoves) {
-            problem.applyMove(move);
-            ParallelBacktracking parallelBacktracking = new ParallelBacktracking(problem, foundSolution, solutions);
-            tasks.add(parallelBacktracking);
-            problem.undoMove(move);
-        }
-        forkJoinPool.invokeAll(tasks);
-
-        while (!solutions.isEmpty()) {
-            System.out.println(solutions.poll());
-        }
-
-        // 2. Camino Mas Corto
-        System.out.println("\n--- Ejecutando Camino Mas Corto ---");
+        System.out.println("--- Ejecutando Camino Mas Corto ---");
         ShortestPathProblem pathProblem = new MyShortestPathProblem(); 
         int initialBestPathLength = Integer.MAX_VALUE;
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        
         ParallelShortestPath parallelShortestPath = new ParallelShortestPath(pathProblem, initialBestPathLength);
         
         long startTime = System.nanoTime();
