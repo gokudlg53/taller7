9737d5e5d17baca847847b7742b13c11d9dd7ee3
gokudlg53 <minicarlosganador@gmail.com>
2026-05-29 22:04:24 -0400

    Update MyBacktrackingProblem.java

--- MyBacktrackingProblem.java
+++ MyBacktrackingProblem.java
@@ -1,38 +1,59 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyBacktrackingProblem implements BacktrackingProblem {
-    private int step = 0;
-    private final int targetSteps = 5;
+public class MyShortestPathProblem implements ShortestPathProblem {
+    private final List<Integer> currentPath;
+    private final int maxDepth = 4;
+
+    public MyShortestPathProblem() {
+        this.currentPath = new ArrayList<>();
+    }
+
+    public MyShortestPathProblem(List<Integer> path) {
+        this.currentPath = new ArrayList<>(path);
+    }
 
     @Override
     public boolean isSolution() {
-        return step == targetSteps;
+        return currentPath.size() == maxDepth;
     }
 
     @Override
     public void applyMove(int move) {
-        step++;
+        currentPath.add(move);
     }
 
     @Override
     public void undoMove(int move) {
-        step--;
+        if (!currentPath.isEmpty()) {
+            currentPath.remove(currentPath.size() - 1);
+        }
     }
 
     @Override
     public List<Integer> getPossibleMoves() {
         List<Integer> moves = new ArrayList<>();
-        if (step < targetSteps) {
-            moves.add(1);
-            moves.add(2);
-            moves.add(3);
+        if (currentPath.size() < maxDepth) {
+            // Rango de 1 a 15
+            for (int i = 1; i <= 15; i++) {
+                moves.add(i);
+            }
         }
         return moves;
     }
 
     @Override
-    public Object getSolution() {
-        return "Solucion General Encontrada a profundidad " + targetSteps;
+    public List<Integer> getCurrentPath() {
+        return new ArrayList<>(currentPath);
+    }
+
+    @Override
+    public int getCurrentPathLength() {
+        int sum = 0;
+        for (int move : currentPath) {
+            // Costo directo: el valor del número es su costo
+            sum += move;
+        }
+        return sum;
     }
 }
