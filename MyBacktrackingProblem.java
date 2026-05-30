79af2d7767ad6088a33c8c4178a5fac4e05acd65
gokudlg53 <minicarlosganador@gmail.com>
2026-05-29 22:17:50 -0400

    Update MyBacktrackingProblem.java

--- MyBacktrackingProblem.java
+++ MyBacktrackingProblem.java
@@ -2,47 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyBacktrackingProblem implements BacktrackingProblem {
-    private final List<Integer> currentPath;
-    private final int maxDepth = 4;
-
-    public MyBacktrackingProblem() {
-        this.currentPath = new ArrayList<>();
-    }
-
-    public MyBacktrackingProblem(List<Integer> path) {
-        this.currentPath = new ArrayList<>(path);
-    }
+    private int step = 0;
+    private final int targetSteps = 5;
 
     @Override
     public boolean isSolution() {
-        return currentPath.size() == maxDepth;
+        return step == targetSteps;
     }
 
     @Override
     public void applyMove(int move) {
-        currentPath.add(move);
+        step++;
     }
 
     @Override
     public void undoMove(int move) {
-        if (!currentPath.isEmpty()) {
-            currentPath.remove(currentPath.size() - 1);
-        }
+        step--;
     }
 
     @Override
     public List<Integer> getPossibleMoves() {
         List<Integer> moves = new ArrayList<>();
-        if (currentPath.size() < maxDepth) {
-            for (int i = 1; i <= 5; i++) { // Ajusta el rango según tu ejercicio original
-                moves.add(i);
-            }
+        if (step < targetSteps) {
+            moves.add(1);
+            moves.add(2);
+            moves.add(3);
         }
         return moves;
     }
 
     @Override
-    public List<Integer> getCurrentPath() {
-        return new ArrayList<>(currentPath);
+    public Object getSolution() {
+        return "Solucion General Encontrada a profundidad " + targetSteps;
     }
 }
