4945ad85f479f413e9aada1cd1bdb2c30660680e
gokudlg53 <minicarlosganador@gmail.com>
2026-05-29 22:15:25 -0400

    Update MainShortestPath.java

--- MainShortestPath.java
+++ MainShortestPath.java
@@ -3,30 +3,27 @@ import java.util.concurrent.ForkJoinPool;
 
 public class MainShortestPath {
     public static void main(String[] args) {
-        System.out.println("--- Ejecutando Camino Mas Corto ---");
-        ShortestPathProblem pathProblem = new MyShortestPathProblem(); 
+        System.out.println("--- Ejecutando Camino Mas Corto (Rango 1-15) ---");
+        ShortestPathProblem pathProblem = new MyShortestPathProblem();
         int initialBestPathLength = Integer.MAX_VALUE;
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        
+
         ParallelShortestPath parallelShortestPath = new ParallelShortestPath(pathProblem, initialBestPathLength);
-        
+
         long startTime = System.nanoTime();
         List<Integer> shortestPath = forkJoinPool.invoke(parallelShortestPath);
         long endTime = System.nanoTime();
 
         if (shortestPath != null) {
             System.out.println("Camino mas corto (paso a paso): " + shortestPath);
-            
-            // Cálculo del costo total para verificación
             int costoTotal = 0;
-            for(int move : shortestPath) {
-                costoTotal += (11 - move);
+            for (int move : shortestPath) {
+                costoTotal += move;
             }
             System.out.println("Costo total calculado: " + costoTotal);
         } else {
             System.out.println("No se encontro ningun camino.");
         }
-        
         System.out.println("Tiempo de ejecucion: " + (endTime - startTime) / 1000000.0 + " ms");
     }
 }
